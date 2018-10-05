c41d39e6519c89e36343593ccf68da872c222cba
Bruno Zaccariello <30813635+bruno-zaccariello@users.noreply.github.com>
2018-10-05 15:17:15 -0300

    Create vote.py

---
+++ modules/vote.py
@@ -0,0 +1,39 @@
+import discord
+import asyncio
+
+class Vote():
+  def __init__(
+    self, goal: int, objective: str, client, author):
+    
+    self.goal = goal
+    self.objective = objective
+    self.client = client
+    self.author = author
+    self.status = True
+    self.voters = []
+    self.total = 1
+
+  async def voting(goal: int, objective: str, client, author):
+    
+    def check(msg):
+      return msg.content.startswith('!up')
+    
+    while True:
+      vote = await client.wait_for_message(timeout=60*5,check=check)
+      if not vote:
+        self.status = False
+        return False
+      if vote.author not in self.voters:
+        self.total += 1
+        if self.total == self.goal:
+          msg = f"\
+          Votação encerrada !\n{self.objective} - {self.total}/{self.goal}"
+          return True
+        self.voters.append(vote.author)
+        msg = f"""
+        {vote.author} Votou !
+        Votos: {self.total}/{self.goal}
+
+        digite !up para votar
+        """
+        await self.client.send_message(vote.channel, msg)
