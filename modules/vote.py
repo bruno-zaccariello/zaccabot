73097233c4093cfd7b0abb24ad844a5142c280cf
Bruno Zaccariello <30813635+bruno-zaccariello@users.noreply.github.com>
2018-10-05 15:38:57 -0300

    Update vote.py

--- modules/vote.py
+++ modules/vote.py
@@ -3,26 +3,27 @@ import asyncio
 
 class Vote():
   def __init__(
-    self, goal: int, objective: str, client, author):
+    self, goal: int, objective: str, client, author, channel):
     
     self.goal = goal
     self.objective = objective
     self.client = client
     self.author = author
-    self.status = True
-    self.voters = []
+    self.voters = [author]
     self.total = 1
+    self.channel = channel
 
   async def voting(self):
     print('Iniciou')
     def check(msg):
-      return msg.content.startswith('!up')
+      return msg.content.startswith('!up') and msg.channel == self.channel
     
+    await self.client.send_message(self.channel, f'Votação {self.objective} iniciada. {self.total}/{self.goal}')
+    await self.client.send_message(self.channel, 'Utilize !up para votar')
     while True:
       print('Aguardando mensagem')
-      vote = await client.wait_for_message(timeout=60*5,check=check)
+      vote = await self.client.wait_for_message(timeout=60*5,check=check)
       if not vote:
-        self.status = False
         return False
       if vote.author not in self.voters:
         self.total += 1
