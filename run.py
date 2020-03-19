03e890e70d8616b36f596f983999b0856294d802
Bruno Zaccariello <30813635+bruno-zaccariello@users.noreply.github.com>
2020-03-19 18:01:23 -0300

    Update run.py

--- run.py
+++ run.py
@@ -77,12 +77,12 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-#async def dicas():
-#    await bot.wait_until_ready()
-#    channel = bot.get_channel('495432153203736576')
-#    while not bot.is_closed:
-#        await bot.send_message(channel, choice(msg_dicas))
-#        await asyncio.sleep(60*60*24)
+async def dicas():
+    await bot.wait_until_ready()
+    channel = bot.get_channel('495432153203736576')
+    while not bot.is_closed:
+        await bot.send_message(channel, choice(msg_dicas))
+        await asyncio.sleep(60*60*24)
 
 bot.loop.create_task(dicas())
 bot.run(TOKEN)
